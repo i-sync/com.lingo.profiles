8324d3691b8ef052c0e31beceb3dfea7de576815
michael <tianzhenyun@gmail.com>
2015-08-16 12:53:57 -0700

    add sql file.

---
+++ profiles.sql
@@ -0,0 +1,60 @@
+Create Database `Profiles`;
+use `Profiles`;
+
+Create Table `Profile`
+(
+	ID int primary key auto_increment,
+    `Name` varchar(64),
+    Avater binary,
+    Email varchar(64),
+    Phone varchar(32),
+    Address varchar(128),
+    Intro varchar(4096)
+);
+
+Create Table Skill
+(
+	ID int primary key auto_increment,
+    PID int ,
+    Title varchar(1024),
+    Content varchar(4096)
+);
+
+Create Table Project 
+(
+	ID int primary key auto_increment,
+    PID int,
+    Title varchar(512),
+    Image binary,
+    Link varchar(128),
+    Tags varchar(256),
+    Intro varchar(4096)
+);
+
+Create Table Education
+(
+	ID int primary key auto_increment,
+    PID int,
+    Title varchar(256),
+    Period varchar(256),
+    Professional varchar(128),
+    Link varchar(128),
+    Intro varchar(4096)
+);
+
+Create Table Living 
+(
+	ID int primary key auto_increment,
+    PID int,
+    Title varchar(256),
+    Content varchar(4096)
+);
+
+Create Table Link
+(
+	ID int primary key auto_increment,
+    PID int,
+    Title varchar(256),
+    Link varchar(128),
+    Logo binary 
+);
