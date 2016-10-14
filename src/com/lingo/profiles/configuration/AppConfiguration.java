ffb01d2e96c26971b3e258b94559e49f8bf12bd0
Michael.Tian <michael.tian@loweprofero.com>
2016-10-14 17:29:33 +0800

    add special static file path.

--- src/com/lingo/profiles/configuration/AppConfiguration.java
+++ src/com/lingo/profiles/configuration/AppConfiguration.java
@@ -57,6 +57,7 @@ public class AppConfiguration extends WebMvcConfigurerAdapter{
         registry.addResourceHandler("/font/**").addResourceLocations("/font/");
         registry.addResourceHandler("/images/**").addResourceLocations("/images/");
         registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+        registry.addResourceHandler("/.*/**").addResourceLocations("/.*/");
     }
      
     @Bean
